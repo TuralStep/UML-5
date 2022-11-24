596b5901dc414dacdc278b08f45d7d7edfeede64
TuralStep <turalozel@gmail.com>
2022-11-24 23:34:31 +0400

    Add project files.

---
+++ Decorator/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Decorator;
+
+
+interface DataSource
+{
+    public void WriteData(string data);
+    public string ReadData();
+}
+
+
+class FileDataSource : DataSource
+{
+    private string _filename;
+
+    public FileDataSource(string filename)
+    {
+        _filename = filename;
+    }
+
+
+    public void WriteData(string data) =>
+        Console.WriteLine($"Data \"{data}\" is written to {_filename} file with FileDataSource...");
+
+    public string ReadData() => "Data from FileDataSource";
+
+}
+
+class DataSourceDecorator : DataSource
+{
+
+    private DataSource _wrappee;
+
+    public DataSourceDecorator(DataSource s)
+    {
+        _wrappee = s;
+    }
+
+    public virtual string ReadData() =>
+        "Data from DataSourceDecorator";
+
+    public virtual void WriteData(string data) =>
+        Console.WriteLine($"Data \"{data}\" is written to file with DataSourceDecorator...");
+}
+
+
+class EncryptionDecorator : DataSourceDecorator
+{
+    public EncryptionDecorator(DataSource s) : base(s) {}
+
+
+    public override string ReadData() =>
+        "Data from EncryptionDecorator";
+
+    public override void WriteData(string data) =>
+        Console.WriteLine($"Data \"{data}\" is written to file with EncryptionDecorator...");
+
+}
+
+class CompressionDecorator : DataSourceDecorator
+{
+    public CompressionDecorator(DataSource s) : base(s) {}
+
+
+    public override string ReadData() =>
+        "Data from CompressionDecorator";
+
+    public override void WriteData(string data) =>
+        Console.WriteLine($"Data \"{data}\" is written to file with CompressionDecorator...");
+
+}
---
+++ Fascade/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Fascade;
+
+
+public class Product
+{
+    public void GetProductDetails()
+    {
+        Console.WriteLine("Fetching the Product Details");
+    }
+}
+
+public class Payment
+{
+    public void MakePayment()
+    {
+        Console.WriteLine("Payment Done Successfully");
+    }
+}
+
+public class Invoice
+{
+    public void SendInvoice()
+    {
+        Console.WriteLine("Invoice Send Successfully");
+    }
+}
+
+
+public class Order
+{
+    public void PlaceOrder()
+    {
+        Product product = new Product(); 
+        Payment payment = new Payment();
+        Invoice invoice = new Invoice();
+
+        Console.WriteLine("Place Order Started");
+
+        product.GetProductDetails();
+        payment.MakePayment();
+        invoice.SendInvoice();
+
+        Console.WriteLine("Order Placed Successfully");
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order();
+        order.PlaceOrder();
+    }
+}
